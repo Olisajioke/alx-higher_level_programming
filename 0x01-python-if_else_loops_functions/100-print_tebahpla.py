d8b2d7be85a91a74d87419975bf81f4f9fdca2ba
olisajioke <chijioke914@mail.com>
2023-08-08 11:47:00 -0700

    Task 9

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    end_char = '\n' if i == ord('a') else ''
     char_to_print = chr(i).lower() if i % 2 == 1 else chr(i).upper()
-    print("{}{}".format(char_to_print, end_char), end="")
+    print(char_to_print, end="")
