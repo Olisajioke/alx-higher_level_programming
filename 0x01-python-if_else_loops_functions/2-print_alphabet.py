4dfe48842214bf4fc42b9bfb02b73d3d608088ea
olisajioke <chijioke914@mail.com>
2023-08-08 04:58:16 -0700

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    end_char = '\n' if i == ord('z') else ''
-    print(chr(i), end=end_char)
+# 2-print_alphabet.py
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
