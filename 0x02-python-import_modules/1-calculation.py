0e03ea8a2555cd362ff7d469ef2bb37acb625974
olisajioke <chijioke914@mail.com>
2023-08-10 14:27:37 -0700

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    """
+    calculating different numbers
+    by importing functions from calculator1.py
+    """
+    from calculator_1 import (add, div, sub, mul)
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
