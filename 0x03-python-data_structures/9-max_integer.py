367e3bab66341f36451d11f7963dd6724362d293
olisajioke <chijioke914@mail.com>
2023-08-12 17:02:23 -0700

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == '':
+    ''' A function to find the maximum integer '''
+    if len(my_list) == 0:
         return(None)
 
     max_integer = 0
