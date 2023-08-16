513f9c972ca6e5f6bdb2e4beff70eb1130163d7b
olisajioke <chijioke914@mail.com>
2023-08-16 12:53:58 -0700

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,10 @@
 def uniq_add(my_list=[]):
     unique_num = set()
     result = 0
+
     for item in my_list:
         if item not in unique_num:
             unique_num.add(item)
             result += item
+
     return(result)
