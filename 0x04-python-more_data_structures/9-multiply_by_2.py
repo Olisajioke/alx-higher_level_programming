c20b26203aa336941b6d8e783975caaaa820b928
olisajioke <chijioke914@mail.com>
2023-08-16 14:59:59 -0700

    Task 12

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    key_list = list(new_dict.keys())
+
+    for key in key_list:
+        new_dict[key] *= 2
+
+    return (new_dict)
