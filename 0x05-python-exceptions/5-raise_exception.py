5c99c76e8ac8c022fb6e8237918bfbdbd2fb3d5e
olisajioke <chijioke914@mail.com>
2023-08-28 12:31:20 -0700

    Task 5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-def trigger_custom_error():
-    """Cause a TypeError exception to be raised."""
-    raise TypeError("Custom error message: Incorrect data type encountered.")
+def raise_exception():
+    raise TypeError("This is a custom TypeError message")
