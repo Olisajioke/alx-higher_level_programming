9d76d6e010c7d8c1fa08c614e9ac7afe6197497a
olisajioke <chijioke914@mail.com>
2023-09-11 18:00:13 -0700

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,19 +1,7 @@
-#!/usr/bin/env python3
-"""Module that defines a class that returns available attributes"""
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """
-    Return a list of available attributes and methods of an object.
-
-    Args:
-    obj (object): The object to inspect.
-
-    Returns:
-    list: A list containing the names of attributes and methods.
-    """
-    attributes_and_methods = []
-    for name in dir(obj):
-        if callable(getattr(obj, name)) or hasattr(obj, name):
-            attributes_and_methods.append(name)
-    return attributes_and_methods
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
