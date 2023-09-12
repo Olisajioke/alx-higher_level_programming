29ea52033db0bab5c5a220948301061b26f8bd13
olisajioke <chijioke914@mail.com>
2023-09-11 18:56:27 -0700

    Task 08

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,54 +1,16 @@
-#!/usr/bin/env python3
-
-class BaseGeometry:
-    """
-    This is a base class for geometry-related operations.
-    """
-
-    def area(self):
-        """
-        Calculate the area of the geometry object.
-
-        Raises:
-        Exception: This method should be implemented in subclasses.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate the value as an integer and
-        check if it's greater than 0.
-
-        Args:
-        name (str): The name of the value being validated.
-        value (int): The value to be validated.
-
-        Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+#!/usr/bin/python3
+"""Defines a class for geometry-related operations"""
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle.
-
-    Attributes:
-    - __width (int): The width of the rectangle.
-    - __height (int): The height of the rectangle.
-    """
+    """A class representing a rectangle."""
 
     def __init__(self, width, height):
-        """
-        Initialize a Rectangle with specified width and height.
+        """Initialize a Rectangle with specified width and height.
 
         Args:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
         """
         self.__width = width
         self.__height = height
