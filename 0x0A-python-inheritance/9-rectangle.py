daff41d6b782dd7e34f1d7861e931b7acd126c47
olisajioke <chijioke914@mail.com>
2023-09-11 19:01:55 -0700

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,53 +1,17 @@
-#!/usr/bin/env python3
-
-class BaseGeometry:
-    """
-    This is a base class for geometry-related operations.
-    """
-
-    def area(self):
-        """
-        Calculate the area of the geometry object.
-
-        Raises:
-        Exception: This method should be implemented in subclasses.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate the value as an integer and check if it's greater than 0.
-
-        Args:
-        name (str): The name of the value being validated.
-        value (int): The value to be validated.
-
-        Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle.
-
-    Attributes:
-    - __width (int): The width of the rectangle.
-    - __height (int): The height of the rectangle.
-    """
+    """A class representing a rectangle."""
 
     def __init__(self, width, height):
-        """
-        Initialize a Rectangle with specified width and height.
+        """Initialize a Rectangle with specified width and height.
 
         Args:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
         """
         self.__width = width
         self.__height = height
@@ -55,19 +19,9 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
 
     def area(self):
-        """
-        Calculate the area of the rectangle.
-
-        Returns:
-        int: The area of the rectangle.
-        """
+        """Returns: int: The area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """
-        Return a string representation of the rectangle.
-
-        Returns:
-        str: A string in the format '[Rectangle] <width>/<height>'.
-        """
+        """Return a string representation of the rectangle."""
         return f"[Rectangle] {self.__width}/{self.__height}"
