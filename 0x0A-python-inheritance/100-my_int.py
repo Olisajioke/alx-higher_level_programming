1ca563e322c147ed7d34e56b938c363c940330fc
olisajioke <chijioke914@mail.com>
2023-09-11 17:01:36 -0700

    tasks

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        Override the equality operator (==) to invert its behavior.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Override the inequality operator (!=) to invert its behavior.
+        """
+        return super().__eq__(other)
