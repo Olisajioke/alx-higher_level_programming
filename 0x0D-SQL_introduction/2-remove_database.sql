98ee3c1f109b7500c36a1a013910926256c96cd6
olisajioke <chijioke914@gmail.com>
2023-10-17 14:44:22 -0700

    task 2

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,8 +1,2 @@
--- Check if the database exists before attempting to delete it
-DO $$ 
-BEGIN
-    IF (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0') > 0 THEN
-        -- Database exists, so drop it
-        DROP DATABASE hbtn_0c_0;
-    END IF;
-END $$;
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
