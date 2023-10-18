29642c361bba57464a31728014795986e23f2a5b
olisajioke <chijioke914@gmail.com>
2023-10-18 13:13:03 -0700

    tasks 1 and 2

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create or modify the user user_0d_1 with all privileges and set the password if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
