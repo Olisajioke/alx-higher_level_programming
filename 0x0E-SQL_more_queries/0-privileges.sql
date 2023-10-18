a32cae9972de5d96f59f2fedd24ed34eb6772103
olisajioke <chijioke914@gmail.com>
2023-10-18 13:08:01 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
+-- This script lists the privileges of MySQL users user_0d_1 and user_0d_2 on the localhost server.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
