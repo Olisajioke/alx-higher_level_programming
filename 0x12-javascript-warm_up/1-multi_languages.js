ac43287864763a7eeaeb80f3173c09707a6f539e
olisajioke <chijioke914@gmail.com>
2023-11-13 12:57:56 -0800

    tasks 0 and 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const cMessage = "C is fun";
-const pythonMessage = "Python is cool";
-const jsMessage = "JavaScript is amazing";
-console.log(cMessage);
-console.log(pythonMessage);
-console.log(jsMessage);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
