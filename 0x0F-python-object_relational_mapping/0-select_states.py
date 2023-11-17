33d9eb7c8f5679a0d4b1db818e1384941dbcb5c1
olisajioke <chijioke914@gmail.com>
2023-11-17 03:39:31 -0800

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
-import mysql.connector
+"""module that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    config = {
-        "host": "localhost",
-        "user": sys.argv[1],
-        "password": sys.argv[2],
-        "database": sys.argv[3],
-        "port": 3306,
-    }
 
-    try:
-        db = mysql.connector.connect(**config)
-        cursor = db.cursor()
-        query = "SELECT * FROM states"
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-        db.close()
-    except mysql.connector.Error as error:
-        print("Error:", error)
+if __name__ == "__main__":
+    my_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                            passwd=sys.argv[2], port=3306, db=sys.argv[3])
+    my_cursor = my_db.cursor()
+    my_cursor.execute("SELECT * FROM states")
+    my_rows = my_cursor.fetchall()
+    for row in my_rows:
+        print(row)
+    my_cursor.close()
+    my_db.close()
