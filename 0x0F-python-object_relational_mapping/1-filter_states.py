6f54b506fe8e9ff1d46cc74f63a4b9af9a032540
olisajioke <chijioke914@gmail.com>
2023-11-17 03:49:27 -0800

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
-
+"""A module that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     my_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = my_db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'A%' ORDER BY states.id""")
-    rows = cur.fetchall()
-
-    for row in rows:
+                         passwd=sys.argv[2],port=3306, my_db=sys.argv[3])
+    my_cursor = my_db.cursor()
+    my_cursor.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    my_rows = my_cursor.fetchall()
+    for row in my_rows:
         print(row)
-
-    cur.close()
+    my_cursor.close()
     my_db.close()
