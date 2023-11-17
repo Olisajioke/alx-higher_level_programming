c44fe0b968d022ccf87e717d8aa8a97cdc403cd0
olisajioke <chijioke914@gmail.com>
2023-11-17 04:58:14 -0800

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,6 @@
 """
 Defines a State class and creates a corresponding MySQL table using SQLAlchemy
 """
-
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,7 +10,11 @@ Base = declarative_base()
 
 class State(Base):
     """
-    Represents the 'states' table in MySQL
+    Represents the 'states' table in MySQL.
+
+    Attributes:
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
@@ -20,7 +23,6 @@ class State(Base):
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://username:password@localhost:3306/database_name',
-        echo=True
+        'mysql+mysqldb://username:password@localhost:3306/database_name', echo=True
     )
     Base.metadata.create_all(engine)
