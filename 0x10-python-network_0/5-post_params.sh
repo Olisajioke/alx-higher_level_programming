8b8420cb07a114a7af0a51367d615d0a16783c3c
olisajioke <chijioke914@gmail.com>
2023-11-30 13:30:27 -0800

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script to send a POST request to a URL with predefined data using curl
-data="email=test@gmail.com&subject=I will always be here for PLD"
-curl -s -X POST -d "$data" "$1"
+# A bash script to send a POST request with predefined data to a URL using curl
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
