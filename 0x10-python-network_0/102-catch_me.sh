cd0196f43225b09b23ea0bf9446094ca83152b2d
olisajioke <chijioke914@gmail.com>
2023-11-30 13:47:16 -0800

    advanced tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script to make a request causing the server to respond with "You got me!"
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me -o /dev/null
