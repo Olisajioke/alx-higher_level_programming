135e0b387e718f7ac2f368a1401c0544dd4c504b
olisajioke <chijioke914@gmail.com>
2023-11-30 13:18:35 -0800

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script sends a request to a given URL using curl and displays only the size of the response body in bytes
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-response=$(curl -sI "$url" | grep -i content-length | awk '{print $2}' | tr -d '\r' | tr -d '\n')
-echo "$response"
+# This script sends a request to a given URL using curl and displays only the size of the response body 
+curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r\n'
