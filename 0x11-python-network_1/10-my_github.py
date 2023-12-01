8e75b2f85c714248e93093e603d43567127b6e41
olisajioke <chijioke914@gmail.com>
2023-12-01 04:21:36 -0800

    updating task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
-Sends a POST request to http://0.0.0.0:5000/search_user with a letter as
-Displays the id and name if JSON is properly formatted and not empty.
-Handles various response scenarios.
+Uses Basic Authentication with a personal access token to display Github ID.
 """
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    username = sys.argv[1]
+    password = sys.argv[2]
 
-    page = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    try:
-        response = page.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
-        print("None")
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(f"Your user ID is: {user_data['id']}")
+    else:
+        print("Failed to fetch user data. Please check your credentials.")
