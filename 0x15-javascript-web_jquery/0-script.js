74224644ed732135a946a473d6c60c1fae4486b2
Olisajioke <chijioke914@gmail.com>
2024-01-10 11:14:22 -0800

    task 1-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// Changes the text color of the <header> element to red (#FF0000)
+document.querySelector('header')?.setAttribute('style', 'color: #FF0000');
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,3 @@
+/* global $ */
+// Changes the text color of the <header> element to red (#FF0000) using jQuery
+$('header').css('color', '#FF0000');
