7656762bbe8f77aa8fde5f204307a90cb558f243
Olisajioke <chijioke914@gmail.com>
2024-01-10 11:18:40 -0800

    task 2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Changes the text color of the <header> element to red (#FF0000) when clicking on DIV#red_header
+
+$('#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
